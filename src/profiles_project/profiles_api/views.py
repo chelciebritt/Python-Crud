4e1e4a8bcb013ae2bbe509428d81f99d1acf44f8
Chelcie Britt <chelciebritt@Chelcies-MacBook-Pro.local>
2017-10-28 15:24:09 -0600

    Crud-ing

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,11 +4,14 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+from . import serializers
 
 # Create your views here.
 
 class HelloApiView(APIView):
     """Test API view."""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
             """Returns a list of APIView features."""
@@ -21,3 +24,32 @@ class HelloApiView(APIView):
             ]
 
             return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Create a hello message with name."""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(
+            serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handles updates."""
+
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        """Patches request, updates fields."""
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """Deletes an object"""
+
+        return Response({'method': 'delete'})
+        
